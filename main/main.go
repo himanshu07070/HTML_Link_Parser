8c8a797f69460c6a4d716a60e73076cefea57d73
himanshu07070 <36814920+himanshu07070@users.noreply.github.com>
2021-05-08 23:28:46 +0530

    Create main.go

---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"himanshu/link"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filename := flag.String("file", "message.html", "The parsing of HTML file ")
+	flag.Parse()
+	s, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	links, err := link.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, i := range links {
+		fmt.Println("Href: ", i.Href)
+		fmt.Println("Text: ", i.Text)
+	}
+	return
+}
