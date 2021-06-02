fa9d94c7ae4bd9238dd3632855becb63b126b52c
himanshu07070 <36814920+himanshu07070@users.noreply.github.com>
2021-06-02 20:01:03 +0530

    Update parse.go

--- link/parse.go
+++ link/parse.go
@@ -7,15 +7,13 @@ import (
 	"golang.org/x/net/html"
 )
 
-// Link represents a link (<a href="...">) in an HTML
-// document.
+// Link represents a link (<a href="...">) in an HTML document.
 type Link struct {
 	Href string
 	Text string
 }
 
-// Parse will take in an HTML document and will return a
-// slice of links parsed from it.
+// Parse will take in an HTML document and will return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
 	if err != nil {
